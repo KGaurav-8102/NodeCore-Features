6aaa0bbb5f974af7eef8c046c7c61a71c860cbf3
Gaurav Yadav <gk5480325@gmail.com>
2022-06-14 09:15:43 +0530

    Inspector module has done

---
+++ inspector.js
@@ -0,0 +1,22 @@
+//CPU Profiler
+
+const inspector = require('inspector');
+const fs = require('fs');
+
+const session = new inspector.Session();
+session.connect();
+
+session.post('Profiler.enable', () => {
+    session.post('Profiler.start', () => {
+      // Invoke business logic under measurement here...
+  
+      // some time later...
+      session.post('Profiler.stop', (err, { profile }) => {
+        // Write profile to disk, upload, etc.
+        if (!err) {
+          fs.writeFileSync('./profile.cpuprofile', JSON.stringify(profile));
+        }
+      });
+    });
+  });
+
