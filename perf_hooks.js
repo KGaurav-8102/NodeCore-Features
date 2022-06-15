e606132c60e8a6777ae678746dc66d5b8d95a1e1
Gaurav Yadav <gk5480325@gmail.com>
2022-06-15 07:35:41 +0530

    Event Loop Delay Monitor Done

--- perf_hooks.js
+++ perf_hooks.js
@@ -1,5 +1,5 @@
 
-const { performance, PerformanceObserver } = require('perf_hooks');
+//onst { performance, PerformanceObserver } = require('perf_hooks');
 
 /* const someFunction = () => {
     console.log('Hello World!');
@@ -22,7 +22,7 @@ wrapped(); */
 
 //Performance Observer entry list
 
-const obs = new PerformanceObserver((perfObserverlist, observer) => {
+/*const obs = new PerformanceObserver((perfObserverlist, observer) => {
     console.log(perfObserverlist.getEntries());
     performance.clearMarks();
     performance.clearMeasures();
@@ -31,4 +31,19 @@ const obs = new PerformanceObserver((perfObserverlist, observer) => {
 
 obs.observe({ type: 'mark'});
 performance.mark('test');
-performance.mark('meow');
+performance.mark('meow'); */
+
+//Performnace Hook monitor event lopp delay
+
+const { monitorEventLoopDelay } = require('perf_hooks');
+const h = monitorEventLoopDelay({ resolution: 20});
+
+h.enable();
+h.disable();
+console.log(h.min);
+console.log(h.max);
+console.log(h.mean);
+console.log(h.stddev);
+console.log(h.percentiles);
+console.log(h.percentile(50));
+console.log(h.percentile(99));
