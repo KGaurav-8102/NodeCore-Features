f67a711153547e49aaf9b9eeccc7b9cc76f5fd7e
Gaurav Yadav <gk5480325@gmail.com>
2022-06-19 10:11:15 +0530

    constructor new vm.script initialisted

---
+++ vm.js
@@ -0,0 +1,22 @@
+// Node.js program to demonstrate the	
+// Constructor: new vm.Script() method
+
+// Including vm and util module
+const vm = require('vm');
+
+// Creating context
+const context = {
+number: 2
+};
+
+// Calling the constructor
+const script = new vm.Script('Type = "Int"; number *= 2;');
+
+// Contextifying object
+vm.createContext(context);
+
+// Calling runInContext method
+script.runInContext(context);
+
+// Displays output
+console.log(context);
